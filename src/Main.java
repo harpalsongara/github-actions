e809a5c8b593d456a59bc31eb84fd2a91347a675
Harpal <harpal.songara.sbg@gmail.com>
2023-10-14 18:26:55 +0530

    ititial commit for this project

--- src/Main.java
+++ src/Main.java
@@ -15,3 +15,10 @@ public class Main {
         }
     }
 }
+/*echo "# github-actions" >> README.md
+        git init
+        git add README.md
+        git commit -m "first commit"
+        git branch -M main
+        git remote add origin git@github.com:harpalsongara/github-actions.git
+        git push -u origin main*/
